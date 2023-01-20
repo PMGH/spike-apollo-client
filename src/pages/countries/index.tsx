1d37f3272f90ce2ff3378fd4c56ea4d3889b762c
Peter McCready <peter.mccready@ooni.com>
2023-01-20 12:18:43 +0000

    static and dynamic data requests with apollo
    WIP suspense

---
+++ src/pages/countries/index.tsx
@@ -0,0 +1,73 @@
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import { FeaturedCountryProps, StaticCountriesListProps } from "types/countries.types";
+import client from "../../../apollo/apollo-client";
+import { useQuery } from "@apollo/client";
+import { GET_COUNTRIES_QUERY, GET_COUNTRY_BY_ID_QUERY } from "apollo/queries";
+import ClientOnly from "components/client-only";
+
+const StaticCountriesList = ({ countries }: StaticCountriesListProps) => {
+  if (countries) {
+    return (
+      <ul>
+        {countries.map(country => <li key={country.code}>{country.name}</li>)}
+      </ul>
+    )
+  }
+  return <p>Countries not found.</p>
+}
+
+const DynamicFeaturedCountry = ({ code }: FeaturedCountryProps) => {
+  const { loading, data: dynamicData, error } = useQuery(GET_COUNTRY_BY_ID_QUERY, { variables: { code } })
+
+
+  console.log({ loading, dynamicData, error  })
+
+  if (loading) {
+    return <span>Loading...</span>
+  }
+
+  if (error) {
+    return <span>Error: {error.message}</span>
+  }
+
+  if(dynamicData?.country) {
+    return <p>Featured Country: {dynamicData?.country?.name}</p>
+  }
+  return null;
+}
+
+const CountriesPage = ({ countries: staticCountries }: any) => {
+  console.log({ staticCountries })
+
+  return (
+    <>
+    <h1>Countries Page</h1>
+      <br />
+      <ClientOnly>
+        <ErrorBoundary fallback={<h2>Could not fetch featured country</h2>}>
+          <Suspense fallback={<h1>Suspense fallback...</h1>}>
+            <DynamicFeaturedCountry code="GE" />
+          </Suspense>
+        </ErrorBoundary>
+      </ClientOnly>
+      <br />
+      <h3>Countries</h3>
+      <StaticCountriesList countries={staticCountries} />
+    </>
+  )
+}
+
+export async function getStaticProps() {
+  const { data } = await client.query({
+    query: GET_COUNTRIES_QUERY
+  });
+
+  return {
+    props: {
+      countries: data.countries.slice(0, 4),
+    },
+  };
+}
+
+export default CountriesPage;
